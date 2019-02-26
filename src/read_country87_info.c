ae39f88748a06625f783ae227f98fcf6990d3e9c
adivi <adivittorio@lbl.gov>
2019-02-25 16:47:12 -0800

    updated for approved license and copyright; added file with third party licenses and ownership

--- src/read_country87_info.c
+++ src/read_country87_info.c
@@ -21,11 +21,27 @@
  integer error code: OK = 0, otherwise a non-zero error code
  
  Created by Alan Di Vittorio on 30 July 2013
- Copyright 2018 Alan Di Vittorio, Lawrence Berkeley National Laboratory, All rights reserved
+ 
+ Moirai Land Data System (Moirai) Copyright (c) 2019, The
+ Regents of the University of California, through Lawrence Berkeley National
+ Laboratory (subject to receipt of any required approvals from the U.S.
+ Dept. of Energy).  All rights reserved.
+ 
+ If you have questions about your rights to use or distribute this software,
+ please contact Berkeley Lab's Intellectual Property Office at
+ IPO@lbl.gov.
+ 
+ NOTICE.  This Software was developed under funding from the U.S. Department
+ of Energy and the U.S. Government consequently retains certain rights.  As
+ such, the U.S. Government has been granted for itself and others acting on
+ its behalf a paid-up, nonexclusive, irrevocable, worldwide license in the
+ Software to reproduce, distribute copies to the public, prepare derivative
+ works, and perform publicly and display publicly, and to permit other to do
+ so.
  
  This file is part of Moirai.
  
- Moirai is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. Moirai is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with Moirai (/moirai/docs/COPYING.txt). If not, see <https://www.gnu.org/licenses/>.
+ Moirai is free software: you can use it under the terms of the modified BSD-3 license (see …/moirai/license.txt)
  
  Modified oct 2015 to account for new country base
  
