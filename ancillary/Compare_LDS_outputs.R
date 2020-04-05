1063eb79d30c70ec7fa76dd18e27b267fd606a51
Kanishka Narayan <37234086+kanishkan91@users.noreply.github.com>
2020-04-05 18:41:29 -0400

    Updating LDS comparison scripts

--- ancillary/Compare_LDS_outputs.R
+++ ancillary/Compare_LDS_outputs.R
@@ -11,6 +11,11 @@ Updated_LDS_Data<-read.csv("D:/MOIRAI_3.1_Final/moirai/ancillary/Land_type_area_
 MOIRAI_Types_old<-read.csv("D:/MOIRAI/moirai/outputs/basins235/MOIRAI_land_types.csv",skip=4)
 #4. New land types
 MOIRAI_Types_new<-read.csv("D:/MOIRAI_3.1_Final/moirai/outputs/basins235_lulcc/MOIRAI_land_types.csv",skip = 4)
+#5. Old carbon file
+Old_Carbon_Data<- read.csv("D:/MOIRAI/moirai/outputs/basins235/Ref_veg_carbon_Mg_per_ha.csv",skip = 5)
+#6. New carbon file
+New_Carbon_Data<- read.csv("D:/MOIRAI_3.1_Final/moirai/outputs/basins235_lulcc/Ref_veg_carbon_Mg_per_ha.csv",skip = 5)
+
 
 
 #Add fast group_by function
@@ -72,24 +77,80 @@ test_that("Compare difference by year and value",{
 
 #Compare Hyde type
 Original_LDS_Data %>%
-  left_join(MOIRAI_Types_old %>% select(Category,LT_HYDE) %>% rename(land_type=Category),by=c("land_type")) %>%
-  fast_group_by(by=c("iso","glu_code","LT_HYDE","year"),colname = "value",func="sum") %>%
-  select(iso,glu_code,year,LT_HYDE,value) %>%
+  left_join(MOIRAI_Types_old %>% select(Category,LT_HYDE,LT_SAGE) %>% rename(land_type=Category),by=c("land_type")) %>%
+  fast_group_by(by=c("iso","glu_code","LT_HYDE","year","LT_SAGE"),colname = "value",func="sum") %>%
+  select(iso,glu_code,year,LT_HYDE,value,LT_SAGE) %>%
   distinct() %>%
   filter(value>2)->ISO_GLU_LT_Year_Data_Old
 
 Updated_LDS_Data %>%
-  left_join(MOIRAI_Types_new %>% select(Category,LT_HYDE) %>% rename(land_type=Category),by=c("land_type")) %>%
-  fast_group_by(by=c("iso","glu_code","LT_HYDE","year"),colname = "value",func="sum") %>%
-  select(iso,glu_code,year,LT_HYDE,value) %>%
+  left_join(MOIRAI_Types_new %>% select(Category,LT_HYDE,LT_SAGE) %>% rename(land_type=Category),by=c("land_type")) %>%
+  fast_group_by(by=c("iso","glu_code","LT_HYDE","year","LT_SAGE"),colname = "value",func="sum") %>%
+  select(iso,glu_code,year,LT_HYDE,value,LT_SAGE) %>%
   rename(Updated_Value=value) %>%
   distinct() %>%
   filter(Updated_Value>2)->ISO_GLU_LT_Year_Data_New
 
-ISO_GLU_LT_Year_Data_Old %>% left_join(ISO_GLU_LT_Year_Data_New,by=c("iso","glu_code","LT_HYDE","year")) %>%
-  mutate(Difference=Updated_Value-value) %>%
+ISO_GLU_LT_Year_Data_Old %>% left_join(ISO_GLU_LT_Year_Data_New,by=c("iso","glu_code","LT_HYDE","year","LT_SAGE")) %>%
+  mutate(Difference=abs(Updated_Value-value)) %>%
   mutate(Difference_Percent=(Difference/value)*100) %>%
-  filter(value>50)->tmp
+  filter(value>5)->tmp
 
-write.csv(tmp,"Hyde_Differences.csv")
+g<-ggplot(data=tmp,aes(x=value,y=Updated_Value))+
+   geom_point()+
+   ggtitle("Comparing LDS outputs between old and new across all years")+
+   labs(subtitle = "This is a comparison at the lowest level i.e Year+ISO+GLU+SAGE_type+Hyde_type")
 
+g
+
+tmp %>% filter(Difference>5) %>% filter(Difference_Percent>15)->Data_for_comparison
+
+
+test_that("Compare difference at lowest level",{
+  
+  expect_equal(sum(nrow(Data_for_comparison)),0,info=paste("Differences at the lowest level are not reasonable"))
+  
+})
+
+tmp %>% filter(year==1990) %>% filter(iso=="idn")->Indonesia_anomaly
+write.csv(Indonesia_anomaly,"Indonesia_anomaly.csv")
+
+
+#Part 2: Carbon accounting for soil_carbon, vegetation carbon
+Old_Carbon_Data %>% 
+  inner_join(Original_LDS_Data %>% 
+               filter(year==2015) %>%
+               select(iso,glu_code,land_type,value) %>% rename(Land_value=value), by=c("iso","glu_code","land_type")) %>% 
+  mutate(Old_Carbon_Total = Land_value * value ) %>% 
+  group_by(iso,glu_code,c_type) %>% 
+  mutate(Old_Carbon_Total=sum(Old_Carbon_Total)) %>% 
+  select(iso,glu_code,c_type,Old_Carbon_Total) %>% 
+  distinct()->Old_Total_Carbon
+                                                                  
+New_Carbon_Data %>% 
+  inner_join(Updated_LDS_Data %>% 
+               filter(year==2015) %>%
+               select(iso,glu_code,land_type,value) %>% rename(Land_value=value), by=c("iso","glu_code","land_type")) %>% 
+  mutate(New_Carbon_Total = Land_value * value ) %>% 
+  group_by(iso,glu_code,c_type) %>% 
+  mutate(New_Carbon_Total=sum(New_Carbon_Total)) %>% 
+  select(iso,glu_code,c_type,New_Carbon_Total) %>% 
+  distinct()->New_Total_Carbon                                   
+
+New_Total_Carbon %>% 
+  inner_join(Old_Total_Carbon,by=c("iso","glu_code","c_type")) %>%
+  mutate(New_Carbon_Total=ifelse(is.na(New_Carbon_Total),0,New_Carbon_Total)) %>% 
+  mutate(Difference=abs(New_Carbon_Total-Old_Carbon_Total)) %>% 
+  mutate(Percent_Difference=(Difference/Old_Carbon_Total)*100)->Carbon_Comparison
+
+g<-ggplot(data=Carbon_Comparison,aes(x=Old_Carbon_Total,y=New_Carbon_Total))+
+  geom_point()+
+  ggtitle("Comparing carbon outputs between old and new across all years")+
+  labs(subtitle = "This is a comparison at the lowest level i.e Year+ISO+GLU+SAGE_type+Hyde_type")+
+  facet_wrap(~c_type)
+
+g
+
+Carbon_Comparison %>% filter(Difference>4000000) %>% 
+                      filter(Percent_Difference>15)->Carbon
+Carbon_Comparison %>% filter(iso=="ecu") %>%filter(c_type=="soil_c")->Ecuador_example 
