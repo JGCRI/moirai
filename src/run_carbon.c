aaf532d323ecd8e4b5d654c4388231392f5aa8e3
PerryMapping <103071930+PerryMapping@users.noreply.github.com>
2023-03-14 14:13:10 -0700

    Update from review
    Carbon bool changed to int.  Debugging messages set to only print if input diagnostics flag set to 1.

--- src/run_carbon.c
+++ src/run_carbon.c
@@ -3,22 +3,27 @@
 
 #include "moirai.h"
 #include <stdio.h>
-#include <stdbool.h>
+
 
 char false_string[6];
 char true_string[5];
-bool carbon_enabled_bool;
+int diagnostics;
+int carbon_enabled_bool;
 
-bool run_carbon(args_struct in_args) { 
-    if (in_args.carbon_enabled == true) {
-        fprintf(fplog, "carbon_enabled_bool in input structure is set to enabled with value to: %d\n", in_args.carbon_enabled);
-        carbon_enabled_bool = true;
+int run_carbon(args_struct in_args) { 
+    if (in_args.carbon_enabled == 1) {
+        carbon_enabled_bool = 1;
         strcpy(true_string, "true");
+        if (diagnostics == 1) {
+            fprintf(fplog, "carbon_enabled_bool in input structure is set to enabled with value to: %d\n", in_args.carbon_enabled);
+            }
         } 
     else {
-        fprintf(fplog, "carbon_enabled_bool in input structure is set to disabled with value %d\n", in_args.carbon_enabled);
-        carbon_enabled_bool = false;
+        carbon_enabled_bool = 0;
         strcpy(false_string, "false");
+        if (diagnostics == 1) {
+            fprintf(fplog, "carbon_enabled_bool in input structure is set to disabled with value %d\n", in_args.carbon_enabled);
+            }
         }
-    fprintf(fplog, "carbon_enabled_bool variable value in run_carbon.c is: %s %s\n", false_string, true_string); // can comment out if not debugging
+    fprintf(fplog, "Carbon processing is set to: %s %s\n", false_string, true_string);
 }
